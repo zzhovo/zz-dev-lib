f6e04e56609419045c2982d20182199a27006634
Hovhannes Ghurshudyan <zz.hovo@gmail.com>
2020-10-17 02:07:16 +0400

    unset user preference functionality

--- user-preference/class-zdl-user-preference-dao.php
+++ user-preference/class-zdl-user-preference-dao.php
@@ -14,8 +14,8 @@ final class ZDL_User_Preference_Dao extends ZDL_Dao implements ZDL_I_User_Prefer
 	/**
 	 * @return ZDL_User_Preference_Dao
 	 */
-	static public function get_instance(){
-		if( null === self::$instance ){
+	static public function get_instance() {
+		if( null === self::$instance ) {
 			self::$instance = new self();
 		}
 
@@ -42,12 +42,9 @@ final class ZDL_User_Preference_Dao extends ZDL_Dao implements ZDL_I_User_Prefer
 	protected $user_preferences = array();
 
 	/**
-	 * @noinspection PhpDocMissingThrowsInspection
-	 *
 	 * @param $user_id
 	 */
-	protected function cache_user_preferences( $user_id ){
-		/** @noinspection PhpUnhandledExceptionInspection */
+	protected function cache_user_preferences( $user_id ) {
 		$preferences = $this->get_many_properties_by_values( array( 'user_id' => $user_id ), array( 'name', 'value' ) );
 
 		$this->user_preferences[ $user_id ] = array();
@@ -59,7 +56,7 @@ final class ZDL_User_Preference_Dao extends ZDL_Dao implements ZDL_I_User_Prefer
 	/**
 	 * @param int   $user_id
 	 */
-	protected function flush_user_preference_cache( $user_id ){
+	protected function flush_user_preference_cache( $user_id ) {
 		unset( $this->user_preferences[ $user_id ] );
 	}
 
@@ -69,20 +66,18 @@ final class ZDL_User_Preference_Dao extends ZDL_Dao implements ZDL_I_User_Prefer
 	 *
 	 * @return string|null    in the case the preference is not set null will be returned
 	 */
-	public function get_preference( $preference_name, $user_id ){
-		if( ! array_key_exists( $user_id, $this->user_preferences ) ){
+	public function get_preference( $preference_name, $user_id ) {
+		if( ! array_key_exists( $user_id, $this->user_preferences ) ) {
 			$this->cache_user_preferences( $user_id );
 		}
 
-		if( array_key_exists( $preference_name, $this->user_preferences[ $user_id ] ) ){
+		if( array_key_exists( $preference_name, $this->user_preferences[ $user_id ] ) ) {
 			return $this->user_preferences[ $user_id ][ $preference_name ];
 		}
 
 		return null;
 	}
 	/**
-	 * @noinspection PhpDocMissingThrowsInspection
-	 *
 	 * @param string    $preference_name
 	 * @param string    $preference_value
 	 * @param int       $user_id
@@ -90,20 +85,44 @@ final class ZDL_User_Preference_Dao extends ZDL_Dao implements ZDL_I_User_Prefer
 	 *
 	 * @return bool
 	 */
-	public function set_preference( $preference_name, $preference_value, $user_id, $flush_cache = true ){
+	public function set_preference( $preference_name, $preference_value, $user_id, $flush_cache = true ) {
 		$preference = array(
 			'user_id' => $user_id,
 			'name'    => $preference_name,
 			'value'   => $preference_value
 		);
 
-		/** @noinspection PhpUnhandledExceptionInspection */
 		$created = $this->create_single( $preference, true );
 
-		if( true === $flush_cache ){
+		if( true === $flush_cache ) {
 			$this->flush_user_preference_cache( $user_id );
 		}
 
 		return $created;
+	}
+	/**
+	 * @param string    $preference_name
+	 * @param int       $user_id
+	 * @param bool      $flush_cache
+	 *
+	 * @return bool
+	 */
+	public function unset_preference( $preference_name, $user_id, $flush_cache = true ) {
+		$where_values = array(
+			'user_id' => $user_id,
+			'name'    => $preference_name
+		);
+
+		$deleted = $this->delete_many_by_values( $where_values );
+
+		if( false === $deleted ) {
+			return false;
+		}
+
+		if( true === $flush_cache ) {
+			$this->flush_user_preference_cache( $user_id );
+		}
+
+		return true;
 	}
 }
